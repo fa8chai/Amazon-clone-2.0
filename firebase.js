f7ed12050e99f5bc0fb3b2e8db540952bfd3ba48
Fami <anne.wi.sw@gmail.com>
2021-05-22 12:48:00 +0300

    day3

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbj5mJhFj_fEgs9LPss-LzxJln8_9DSGg",
+  authDomain: "fir-db658.firebaseapp.com",
+  projectId: "fir-db658",
+  storageBucket: "fir-db658.appspot.com",
+  messagingSenderId: "693456171309",
+  appId: "1:693456171309:web:2b0349a5362006b146a3a4",
+  measurementId: "G-1H457GJDE7"
+};
