70b935fb2823c7ca1096a6799b5d07f2362a2d7e
Fami <anne.wi.sw@gmail.com>
2021-05-24 01:21:41 +0300

    day4 stripe

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -7,9 +7,7 @@ import Currency from 'react-currency-formatter';
 import { useSession } from "next-auth/client";
 import { loadStripe } from '@stripe/stripe-js';
 import axios from "axios";
-import Stripe from "stripe";
 
-const stripe = Stripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
 function Checkout() {
     const items = useSelector(selectItems);
@@ -18,6 +16,7 @@ function Checkout() {
     const totalItems = useSelector(state => state.basket.items.reduce((total, item) => total + item.quantity , 0));
 
     const createCheckoutSession = async () => {
+        const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
         const CheckoutSession = await axios.post('/api/create-checkout-session', {
             items,
