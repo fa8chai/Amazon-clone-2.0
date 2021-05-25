7375f10fae433489ab7f9903f336c4ca27b139bb
Fami <anne.wi.sw@gmail.com>
2021-05-25 20:36:59 +0300

    a

--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -42,14 +42,14 @@ function ProductPage() {
                 <title>Amazon 2.0 | {product.title}</title>
             </Head>
             <Header />
-        <div className='flex flex-col items-center md:flex-row md:space-x-4 max-w-screen-lg mx-auto mt-10'>
+        <div className='flex flex-col items-center lg:flex-row lg:space-x-4 max-w-screen-lg mx-auto mt-10'>
             <Image
                 src={product.image}
                 height={400}
                 width={400}
                 objectFit='contain'
             />
-            <div className='relative flex flex-col p-5 lg:p-10'>
+            <div className='relative flex flex-col p-5 lg:p-10 flex-grow-0'>
                 <p className='absolute top-2 right-2 text-xs italic text-gray-400'>{product.category}</p>
                 <h1 className='text-3xl mb-5'>{product.title}</h1>
                 <p className='font-bold mx-5'>
@@ -58,7 +58,7 @@ function ProductPage() {
                 />
                 </p>
                 <p className='text-gray-500 my-5'>{product.description}</p>
-                <div className='flex'>
+                <div className='flex ml-5'>
                 {Array(product.rating).fill().map((_, i) => (
                     <StarIcon key={i} className='h-5 text-yellow-500' />
                 ))}
@@ -70,7 +70,7 @@ function ProductPage() {
                 </div>
                 )}
 
-                {!index >= 0 ? (
+                {index < 0 ? (
                     <button onClick={addItemToBasket} className='my-5 button'>Add to Basket</button>
 
                 ) : (
