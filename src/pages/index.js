0d221027d79fe2a59fc83448b5d7be827e8b8e36
Fami <anne.wi.sw@gmail.com>
2021-05-26 16:58:01 +0300

    r

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import ProductFeed from "../components/ProductFeed";
-import { useState } from 'react';
 import Sidebar from "../components/Sidebar";
 import { useDispatch } from "react-redux";
 import { setProducts } from '../slices/basketSlice';
