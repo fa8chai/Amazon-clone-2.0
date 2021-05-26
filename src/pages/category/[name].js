8dc613b97d82c6e029dc10eca56f2009018d1919
Fami <anne.wi.sw@gmail.com>
2021-05-26 18:16:21 +0300

    category page

--- src/pages/category/[name].js
+++ src/pages/category/[name].js
@@ -43,7 +43,7 @@ function CategoryPage({ categories }) {
             <Sidebar categories={filteredCategories} onSearchValue={filterCategories} />
             <div className='w-full'>
                 <Header />
-                <main className='max-w-screen-2xl mx-auto mt-32 pt-2'>
+                <main className='max-w-screen-2xl mx-auto mt-10 md:mt-32  pt-2'>
                 {categoryProducts?.length > 0 ? (
                     <ProductFeed products={categoryProducts} />
                 ) : (
