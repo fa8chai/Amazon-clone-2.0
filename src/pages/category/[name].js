b95f13cf2e06c7d81a7baac071c6d07d9da2ce43
Fami <anne.wi.sw@gmail.com>
2021-05-26 17:33:00 +0300

    tt

--- src/pages/category/[name].js
+++ src/pages/category/[name].js
@@ -4,6 +4,7 @@ import Router from 'next/router'
 import { useEffect, useState } from 'react';
 import Head from "next/head";
 import ProductFeed from "../../components/ProductFeed";
+import ProductFeed from "../../components/ProductFeed";
 
 function CategoryPage({ categories }) {
     const products = useSelector(selectProducts);
