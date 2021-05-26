1ecbd8b70997f84ba557d8daca47bae24514dd33
Fami <anne.wi.sw@gmail.com>
2021-05-26 17:13:09 +0300

    sidebar

--- src/pages/category/[name].js
+++ src/pages/category/[name].js
@@ -5,13 +5,20 @@ import { useEffect, useState } from 'react';
 import Head from "next/head";
 import ProductFeed from "../../components/ProductFeed";
 
-function CategoryPage() {
+function CategoryPage({ categories }) {
     const products = useSelector(selectProducts);
     const category = useSelector(selectCategory);
     const [loaded,setLoaded] = useState(false)
 
     const categoryProducts = products?.filter(product => product.category === category);
     
+    const [filteredCategories, setCategories] = useState(categories);
+    const filterCategories = (searchText) => {
+        const matchedCategories = categories.filter((category) =>
+            category.toLowerCase().includes(searchText.toLowerCase())
+        );
+        setCategories([...matchedCategories]);
+    }
     useEffect(() => {
         if(!category){
 
@@ -27,19 +34,32 @@ function CategoryPage() {
     }
   
     return (
-        <div>
+        <div className='h-full flex'>
             <Head>
                 <title>Amazon 2.0 | {category}</title>
             </Head>
-            <main className='max-w-screen-2xl mx-auto mt-5'>
+            <Sidebar categories={filteredCategories} onSearchValue={filterCategories} />
+            <div className='w-full'>
+                <Header />
+                <main className='max-w-screen-2xl mx-auto mt-5'>
                 {categoryProducts?.length > 0 ? (
                     <ProductFeed products={categoryProducts} />
                 ) : (
                     <h1 className='text-center mx-auto text-2xl py-4'>No products…</h1>
                 )}
             </main>
+            </div>
         </div>
     )
 }
 
 export default CategoryPage
+export async function getServerSideProps(context){
+    const categories = await fetch('https://fakestoreapi.com/products/categories').then(res => res.json())
+    
+    return { props: {
+      categories: categories
+    } 
+  }
+  
+  }
